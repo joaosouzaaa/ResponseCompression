43e025f59206d34d4f33e42bd2bab2177f0e0bcc
joaosouzaaa <joaoasouza982@gmail.com>
2024-10-03 21:23:47 -0300

    Feat: Adding initial projects and response compression.

---
+++ src/ResponseCompression.API/DependencyInjection/ResponseCompressionDependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.ResponseCompression;
+using System.IO.Compression;
+
+namespace ResponseCompression.API.DependencyInjection;
+
+internal static class ResponseCompressionDependencyInjection
+{
+    internal static void AddResponseCompressionDependencyInjection(this IServiceCollection services)
+    {
+        services.AddResponseCompression(options =>
+        {
+            options.EnableForHttps = true;
+            options.Providers.Add<BrotliCompressionProvider>();
+            options.Providers.Add<GzipCompressionProvider>();
+            options.MimeTypes = ResponseCompressionDefaults.MimeTypes;
+        });
+
+        services.Configure<BrotliCompressionProviderOptions>(options =>
+            options.Level = CompressionLevel.Optimal);
+        
+        services.Configure<GzipCompressionProviderOptions>(options =>
+            options.Level = CompressionLevel.SmallestSize);
+    }
+}
---
+++ src/ResponseCompression.API/Program.cs
@@ -0,0 +1,24 @@
+using ResponseCompression.API.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDependencyInjection(configuration);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseResponseCompression();
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
