32866502101b2963ad020553eb942f23fa008d10
joaosouzaaa <joaoasouza982@gmail.com>
2024-10-05 12:56:23 -0300

    Tests: File Service unit tests.

--- tests/UnitTests/ServicesTests/FileServiceTests.cs
+++ tests/UnitTests/ServicesTests/FileServiceTests.cs
@@ -15,5 +15,39 @@ public sealed class FileServiceTests
         _fileService = new FileService(_fileSystemMock.Object);
     }
 
+    [Fact]
+    public async Task GetAllFilesAsync_SuccessfulScenario_ReturnsFileResponseList()
+    {
+        // A
+        const string folderPath = "random";
+        _fileSystemMock.Setup(f => f.Path.Combine(
+            It.IsAny<string[]>()))
+            .Returns(folderPath);
 
+        var files = new string[]
+        {
+            "test",
+            "test123"
+        };
+        _fileSystemMock.Setup(f => f.Directory.GetFiles(
+            It.IsAny<string>()))
+            .Returns(files);
+
+        var fileBytes = new byte[]
+        {
+            00,
+            92
+        };
+        _fileSystemMock.SetupSequence(f => f.File.ReadAllBytesAsync(
+            It.IsAny<string>(),
+            It.IsAny<CancellationToken>()))
+            .ReturnsAsync(fileBytes)
+            .ReturnsAsync(fileBytes);
+
+        // A
+        var fileResponseListResult = await _fileService.GetAllFilesAsync(default);
+
+        // A
+        Assert.Equal(files.Count(), fileResponseListResult.Count);
+    }
 }
